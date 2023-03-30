7db8d77c6c3cad8ca1a0fa73ad849a9429ac3122
Scott Bellware <sbellware@gmail.com>
2023-03-30 15:44:17 -0500

    Validator tests are brought into compliance with TestBench norms

--- test/automated/validation.rb
+++ test/automated/validation.rb
@@ -2,38 +2,38 @@ require_relative 'automated_init'
 
 context 'Validating a given UUID' do
   context 'A valid UUID is passed in' do
-    test 'Returns true' do
+    test 'Valid' do
       identifier = Identifier::UUID::Controls::Random.example
       validated = Identifier::UUID.uuid?(identifier)
 
-      assert(validated == true)
+      assert(validated)
     end
   end
 
   context 'An invalid UUID is passed in' do
-    test 'Returns false' do
+    test 'Not valid' do
       identifier = Identifier::UUID::Controls::Anomaly.example
       validated = Identifier::UUID.uuid?(identifier)
 
-      assert(validated == false)
+      refute(validated)
     end
   end
 
   context 'No UUID is passed in' do
-    test 'Returns false' do
+    test 'Not valid' do
       identifier = '12345678'
       validated = Identifier::UUID.uuid?(identifier)
 
-      assert(validated == false)
+      refute(validated)
     end
   end
 
   context 'Blank string is passed in' do
-    test 'Returns false' do
+    test 'Not valid' do
       identifier = ''
       validated = Identifier::UUID.uuid?(identifier)
 
-      assert(validated == false)
+      refute(validated)
     end
   end
 end
